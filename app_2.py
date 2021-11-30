349a446b8a9df87b7afd71454edc168051377133
Tal Mizrahi <48062018+TalMizrahi@users.noreply.github.com>
2021-11-30 23:56:26 +0200

    Fixed Bug

--- app_2.py
+++ app_2.py
@@ -18,11 +18,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'database.db'
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
-    email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(80))
+# Dosen't work yet:
+# class User(db.Model):
+#     id = db.column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(15), unique=True)
+#     email = db.Column(db.String(50), unique=True)
+#     password = db.Column(db.String(80))
 
 
 class LoginForm (FlaskForm):
